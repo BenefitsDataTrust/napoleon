a61fb6b806e6264e62307ecdfd137f6e8e627960
carlpartridge <carl.partridge@gmail.com>
2018-02-14 11:27:12 -0500

    Remove failing test !?!?

--- spec/napoleon_spec.rb
+++ spec/napoleon_spec.rb
@@ -4,8 +4,5 @@ describe Napoleon do
   it 'has a version number' do
     expect(Napoleon::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
+  
 end
