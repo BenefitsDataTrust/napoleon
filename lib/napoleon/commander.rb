29d748fe3764aa7c7acf099bb0e59f7ab100c494
Lillian Klasen <lklasen@bdtrust.org>
2018-04-17 11:23:06 -0400

    Update method_missing method in commander

--- lib/napoleon/commander.rb
+++ lib/napoleon/commander.rb
@@ -3,11 +3,12 @@ module Napoleon
 
     def method_missing method_key, **args
       perform method_key, **args
+      super
     end
 
     def perform method_key, **args
     	CommandBuilder.new(self).send(method_key.to_s, **args)
     end
-    
+
   end
 end
