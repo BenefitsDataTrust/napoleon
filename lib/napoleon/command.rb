f6a65b27d381965cbe2cb8ac7c18cc82498290d1
carlpartridge <carl.partridge@gmail.com>
2018-07-02 16:20:32 -0400

    Fix flow, todo cleanup, testing

--- lib/napoleon/command.rb
+++ lib/napoleon/command.rb
@@ -3,38 +3,44 @@ require 'napoleon/command_broadcaster'
 module Napoleon
   class Command
 
-    attr_reader :user, :extra_broadcasts, :return_function, :args#, :broadcast_server_response
+    attr_reader :user, :args#, :extra_broadcasts, :return_function#, :broadcast_server_response
 
-    def self.by user
-      new user
-    end
+    # def self.by user
+    #   new user
+    # end
 
     def initialize user:SystemUser.new, **args
       @user = user
       # allows you to generate additional broadcasts, using custom command methods, to custom topics
-      @extra_broadcasts = *args.delete(:extra_broadcasts)
-      # changes the returned object using a custom command method
-      @return_function = args.delete(:return_function)
+      # @extra_broadcasts = *args.delete(:extra_broadcasts)
+      # # changes the returned object using a custom command method
+      # @return_function = args.delete(:return_function)
       @args = args
     end
 
     def enact
-      result = if return_function
-        send(return_function.to_sym, args)
-      else
-        perform args
-      end
+
+      before_command
+
+      result = perform args
+      # result = if return_function
+      #   send(return_function.to_sym, args)
+      # else
+      #   perform args
+      # end
 
       CommandBroadcaster.broadcast(
         object:result, 
         event_name:event_name, 
-        broadcast_override:command_broadcast_override
+        # broadcast_override:command_broadcast_override
       )
 
-      handle_extra_broadcasts(result) if extra_broadcasts.present?
+      # handle_extra_broadcasts(result) if extra_broadcasts.present?
+
+      after_command(result:result)
 
       # Napoleon::CommandResponse.new result, broadcast_server_response
-      result
+      # transform(raw_result:result)
     end
 
     def callback **args
@@ -45,22 +51,33 @@ module Napoleon
       ""
     end
 
+    # def transform_result raw_result:
+    #   raw_result
+    # end
+
+    def before_command
+    end
+
+    def after_command result:
+      result
+    end
+
 
     private
 
-    def handle_extra_broadcasts result
-      extra_broadcasts.each do |callback|
-        send(callback.to_sym, result, args)
-      end
-    end
+    # def handle_extra_broadcasts result
+    #   extra_broadcasts.each do |callback|
+    #     send(callback.to_sym, result, args)
+    #   end
+    # end
 
     def perform
       raise "A command must have a perform method."
     end
 
-    def command_broadcast_override
-      respond_to?(:broadcast!) && broadcast!
-    end
+    # def command_broadcast_override
+    #   respond_to?(:broadcast!) && broadcast!
+    # end
 
   end
 end
--- lib/napoleon/command_broadcaster.rb
+++ lib/napoleon/command_broadcaster.rb
@@ -1,20 +1,26 @@
 module Napoleon
   class CommandBroadcaster
 
-    def self.broadcast object:, channels:["portfolio-events"], event_name:"", broadcast_override:false
+    # def self.broadcast object:, channels:["portfolio-events"], event_name:"", broadcast_override:false
+    #   Napoleon.broadcasters.each { |broadcaster|
+    #     broadcaster.perform(event_name, object, channels) if broadcast?(broadcaster, broadcast_override)
+    #   }
+    # end
+
+    def self.broadcast event_name:"", object:#, channels:["portfolio-events"]#,broadcast_override:false
       Napoleon.broadcasters.each { |broadcaster|
-        broadcaster.perform(event_name, object, channels) if broadcast?(broadcaster, broadcast_override)
+        broadcaster.perform event_name, object#, channels)# if broadcast?(broadcaster, broadcast_override)
       }
     end
 
 
-    private
+    # private
 
-    def self.broadcast? broadcaster, broadcast_override
-      return true if !broadcaster.respond_to?(:broadcast_all_events)
-      return true if broadcaster.broadcast_all_events
-      broadcast_override
-    end
+    # def self.broadcast? broadcaster, broadcast_override
+    #   return true if !broadcaster.respond_to?(:broadcast_all_events)
+    #   return true if broadcaster.broadcast_all_events
+    #   broadcast_override
+    # end
 
   end
 end
